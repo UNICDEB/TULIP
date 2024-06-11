ff23dd3276ab58fcf493bf8c0e5fa75937375a94
UNICDEB <doloidebabrat99@gmail.com>
2024-06-11 23:27:16 +0530

    Added

---
+++ GUI/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+    # # Create Title Label
+        # self.textbox = customtkinter.CTkTextbox(master=self, corner_radius=10)
+        # self.textbox.grid(row=0, column=1, sticky="nsew")
+        # # self.textbox.insert("0.0", "Some example text!\n" * 50)
+        # self.heading = customtkinter.CTkLabel(master=self.textbox, font=("Times New Roman", 20) , justify="center" , text="WELCOME TO TULIP")
+        # self.heading.grid(padx = 0,pady=0, sticky= "nsew")
